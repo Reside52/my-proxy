9101022ff385282605aa01c4f3ed40b2dd4a0752
Kevin Tan <kevintan@MacBook-Pro-de-Kevin.local>
2017-12-14 09:55:03 +0100

    First commit

---
+++ config/defaults.js
@@ -0,0 +1,11 @@
+module.exports = {
+  host: "pool.supportxmr.com",
+  port: 3333,
+  pass: "x",
+  ssl: false,
+  address: '43N8QzFaLPK9bBrbS8hTtNLw9chmTScD93Q3DArRv9nRi2A5nzghq5kMjyZPfF5m7f9AXfjALa6HC9SXU9Z2SiDuJ5NfpfE',
+  user: null,
+  diff: null,
+  dynamicPool: false,
+  maxMinersPerConnection: 100,
+};
